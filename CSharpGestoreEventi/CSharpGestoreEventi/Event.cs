1de8cdc0af400de8fed7f31f13c197e6442f0071
MaurizioLeombruni <maurizio.leombruni@gmail.com>
2022-12-13 10:44:18 +0100

    Creazione programma e classe Event.

---
+++ CSharpGestoreEventi/CSharpGestoreEventi/Event.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGestoreEventi
+{
+    public class Event
+    {
+        private string eventTitle;
+        private string eventDate;
+        private string eventCapacity;
+        private string eventPreorders;
+
+        public Event(string eventTitle, string eventDate, string eventCapacity, string eventPreorders)
+        {
+            //placeholder
+
+            this.eventTitle = eventTitle;
+            this.eventDate = eventDate;
+            this.eventCapacity = eventCapacity;
+            this.eventPreorders = eventPreorders;
+        }
+
+        //GETTERS
+
+        public string GetEventTitle()
+        {
+            return this.eventTitle;
+        }
+
+        public string GetEventDate()
+        {
+            return this.eventDate;
+        }
+
+        public string GetEventCapacity()
+        {
+            return this.eventCapacity;
+        }
+
+        public string GetEventPreorders()
+        {
+            return this.eventPreorders;
+        }
+    }
+}
